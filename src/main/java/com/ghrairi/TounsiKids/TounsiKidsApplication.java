312fd7f7f819a29788e1460b7afd0024ea7586e5
borhen ghrairi <borhen.ghrairi@teamwillgroup.com>
2024-08-12 13:59:24 +0000

    Initial commit

---
+++ src/main/java/com/ghrairi/TounsiKids/TounsiKidsApplication.java
@@ -0,0 +1,13 @@
+package com.ghrairi.TounsiKids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TounsiKidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TounsiKidsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ghrairi/TounsiKids/TounsiKidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghrairi.TounsiKids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TounsiKidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
